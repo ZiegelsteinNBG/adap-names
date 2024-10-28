dc58dbd0bd19fe5b673e3f169da82237f49d8fd1
Ziegelstein <91370891+ZiegelsteinNBG@users.noreply.github.com>
2024-10-28 08:21:12 +0100

    Update Name.ts

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -7,37 +7,57 @@ export class Name {
     private delimiter: string = this.DEFAULT_DELIMITER;
 
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation");
+        if(other !== undefined) this.components = other;
+        if(delimiter !== undefined) this.delimiter = delimiter;
     }
 
     /** Returns human-readable representation of Name instance */
     public asNameString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+        let res: string = '';
+        let size: number = this.getNoComponents();
+        let counter: number = 0;
+
+        while(counter < size){
+            res += this.getComponent(counter);
+            if(counter != size-1){
+                res += delimiter;
+            }
+            counter ++;
+        }
+        return res;
     }
 
     public getComponent(i: number): string {
-        throw new Error("needs implementation");
+        if(!this.isValidIdx(i))return '';
+        return this.components[i];
     }
 
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation");
+        if(this.isValidIdx(i)) this.components[i] = c;
     }
 
-     /** Returns number of components in Name instance */
-     public getNoComponents(): number {
-        throw new Error("needs implementation");
+    protected isValidIdx(i: number): boolean {
+        if(i < 0 || i >= this.getNoComponents()){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public getNoComponents(): number {
+        return this.components.length;
     }
 
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation");
+        if(this.isValidIdx(i)) this.components.splice(i, 0, c);
     }
 
     public append(c: string): void {
-        throw new Error("needs implementation");
+        if (c != undefined) this.components.push(c);
     }
 
     public remove(i: number): void {
-        throw new Error("needs implementation");
+        this.components[i] = '';
     }
 
 }
