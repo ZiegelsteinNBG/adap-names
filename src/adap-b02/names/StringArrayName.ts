78222c6b5d60ccf92818f635f26772a3845a0bf5
Ziegelstein <91370891+ZiegelsteinNBG@users.noreply.github.com>
2024-11-10 17:36:14 +0100

    Update StringArrayName.ts

--- src/adap-b02/names/StringArrayName.ts
+++ src/adap-b02/names/StringArrayName.ts
@@ -16,7 +16,7 @@ export class StringArrayName implements Name {
         let counter: number = 0;
 
         while(counter < size){
-            if(components[i] === "") continue;
+            if(this.components[i] === "") continue;
             res += this.getComponent(counter);
             if(counter != size-1){
                 res += delimiter;
@@ -32,7 +32,7 @@ export class StringArrayName implements Name {
         let counter: number = 0;
 
         while(counter < size){
-            if(components[i] === "") continue;
+            if(this.components[i] === "") continue;
             res += this.insertEscCh(this.getComponent(counter));
             if(counter != size-1){
                 res += DEFAULT_DELIMITER;
