3761214de0ba7dde76df7ca023a99dcabac2c79a
ZiegelsteinNBG <nhatkhangvo00@gmail.com>
2024-11-10 17:08:09 +0100

    Abgabe 02

--- src/adap-b02/names/StringName.ts
+++ src/adap-b02/names/StringName.ts
@@ -7,52 +7,137 @@ export class StringName implements Name {
     protected name: string = "";
     protected length: number = 0;
 
+    // String input is a single component
     constructor(other: string, delimiter?: string) {
-        throw new Error("needs implementation");
+        if(other !== undefined){
+            this.name = other;
+            this.length++;
+        } 
+        if(delimiter !== undefined) this.delimiter = delimiter;
     }
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+        const components = this.splitRespectingEscapes();
+        let str: string = "";
+        for(let i:number = 0; i < components.length; i++){
+            str += this.replaceEscCh(components[i]);
+        }
+        return str;
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation");
+        const components = this.splitRespectingEscapes();
+        let str: string = "";
+        for(let i:number = 0; i < components.length; i++){
+            str += components[i];
+        }
+        return str;
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation");
+        const components = this.splitRespectingEscapes();
+        for(let i:number = 0; i < components.length; i++){
+            if(components[i] !== "" || components[i] !== null){
+                return false;
+            }
+        }
+        return true;
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation");
+        return this.delimiter;
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation");
+        return this.length;
     }
 
     public getComponent(x: number): string {
-        throw new Error("needs implementation");
+        if (x < 0 || x >= this.length) {
+            throw new Error("Index out of bounds");
+        }
+        const components = this.splitRespectingEscapes();
+        return components[x];
     }
 
     public setComponent(n: number, c: string): void {
-        throw new Error("needs implementation");
+        const components = this.splitRespectingEscapes();
+        this.name = "";
+        for(let i:number = 0; i < components.length; i++){
+            if(i === n){
+                this.name += c + this.delimiter;
+            }else{
+                this.name += components[i]+this.delimiter;
+            }
+            
+        }
     }
 
     public insert(n: number, c: string): void {
-        throw new Error("needs implementation");
+        ++this.length;
+        const components = this.splitRespectingEscapes();
+        this.name = "";
+        for(let i:number = 0; i < components.length; i++){
+            if(i === n){
+                this.name += c + this.delimiter;
+            }
+            this.name += components[i]+this.delimiter;
+        }
     }
 
     public append(c: string): void {
-        throw new Error("needs implementation");
+        this.length ++;
+        this.name += this.insertEscCh(this.delimiter);
     }
 
     public remove(n: number): void {
-        throw new Error("needs implementation");
+        const components = this.splitRespectingEscapes();
+        this.name = "";
+        for(let i:number = 0; i < components.length; i++){
+            if(i !== n){
+                this.name += components[i];
+            }
+            this.name += this.delimiter;
+        }
     }
 
     public concat(other: Name): void {
-        throw new Error("needs implementation");
+        this.name += this.delimiter + other.asDataString();
+        this.length +=  other.getNoComponents();
+    }
+
+    private insertEscCh(i: string): string {
+        return i.replaceAll(this.delimiter, ESCAPE_CHARACTER+this.delimiter);
+    }
+
+    private replaceEscCh(i: string): string {
+        return i.replaceAll(ESCAPE_CHARACTER+this.delimiter, this.delimiter);
+    }
+
+    private splitRespectingEscapes(): string[] {
+        const parts: string[] = [];
+        let currentPart = "";
+        let isEscaped = false;
+
+        for (let i = 0; i < this.name.length; i++) {
+            const char = this.name[i];
+
+            if (char === ESCAPE_CHARACTER && !isEscaped) {
+                
+                isEscaped = true;
+            } else if (char === this.delimiter && !isEscaped) {
+
+                parts.push(currentPart);
+                currentPart = "";
+            } else {
+                
+                currentPart += char;
+                isEscaped = false;
+            }
+        }
+        
+        parts.push(currentPart); 
+        return parts;
     }
 
 }
